b748d1655c5af7d2ffbad832e85dfe3e77ebbfaa
Shantanu Nitin Kor <kor.shantanu1@gmail.com>
2024-05-04 19:36:03 +0530

    Changed some things

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <>
-      <h1 className="md:text-4xl text-2xl text-center bg-gradient-to-r from-green-700 via-violet-700 to-indigo-700 text-white p-1 md:p-3">To-Do</h1>
+      <h1 className="md:text-4xl text-2xl text-center bg-gradient-to-r from-green-700 via-violet-700 to-indigo-700 text-white p-1 md:p-3">To-Do Task Tracker</h1>
       <Home />
     </>
   );
